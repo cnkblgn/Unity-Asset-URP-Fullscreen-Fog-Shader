524adcfb983f6ddb1c3b1eef954b7db9fde77510
Cenk Bilgin <cenkbilgin2000@gmail.com>
2024-05-12 18:31:03 +0300

    Commit.

--- Assets/Unity-Asset-URP-Fullscreen-Fog-Shader/Shaders/HLSL_ComputeLight.hlsl
+++ Assets/Unity-Asset-URP-Fullscreen-Fog-Shader/Shaders/HLSL_ComputeLight.hlsl
@@ -1,68 +1,35 @@
 #ifndef HLSLCOMPUTELIGHT_INCLUDED
 #define HLSLCOMPUTELIGHT_INCLUDED
 
-void GetLight_float(float3 worldPos, out float3 direction, out float3 color, out float shadow)
+void Compute_float(float3 worldPosition, out float3 lightDirection, out float3 lightColor)
 {
     #ifdef SHADERGRAPH_PREVIEW
-    direction = normalize(float3(-0.5,0.5,-0.5));
-    color = float3(1,1,1);
-    shadow = 1;
+    lightDirection = normalize(float3(-0.5,0.5,-0.5));
+    lightColor = float3(1,1,1);
     #else
     
-    Light mainLight = GetMainLight(TransformWorldToShadowCoord(worldPos), worldPos, unity_ProbesOcclusion);
-    shadow = mainLight.shadowAttenuation;
-    direction = mainLight.direction;
-    color = mainLight.color;
+    Light mainLight = GetMainLight(TransformWorldToShadowCoord(worldPosition), worldPosition, unity_ProbesOcclusion);
+    lightDirection = mainLight.direction;
+    lightColor = mainLight.color;
     
     #endif
 }
 
-void GetLight_half(half3 worldPos, out half3 direction, out half3 color, out half shadow)
-{
-    #ifdef SHADERGRAPH_PREVIEW
-    direction = normalize(half3(-0.5,0.5,0.5));
-    color = half3(1,1,1);
-    shadow = 1;
-
-    #else
-    
-    Light mainLight = GetMainLight(TransformWorldToShadowCoord(worldPos), worldPos, unity_ProbesOcclusion);
-    shadow = mainLight.shadowAttenuation;
-    direction = mainLight.direction;
-    color = mainLight.color;
-    
-    #endif
-}
-
-void GetLightNoShadow_float(out float3 direction, float3 color)
-{
-    #ifdef SHADERGRAPH_PREVIEW
-    direction = normalize(float3(-0.5,0.5,-0.5));
-    color = float3(1,1,1);
-    #else
-
-    Light mainLight = GetMainLight();
-    direction = mainLight.direction;
-    color = mainLight.color;
-    
-    #endif
-}
-
-void GetLightShadow_float(float3 WorldPos, out half3 Direction, out half3 Color, out half DistanceAtten, out half ShadowAtten)
+void Compute_float(float3 worldPosition, out half3 lightDirection, out half3 lightColor, out half lightDistanceAttenuation, out half lightShadowAttenuation)
 {
    #ifdef SHADERGRAPH_PREVIEW
-   Direction = half3(0.5, 0.5, 0);
-   Color = 1;
-   DistanceAtten = 1;
-   ShadowAtten = 1;
+   lightDirection = half3(0.5, 0.5, 0);
+   lightColor = 1;
+   lightDistanceAttenuation = 1;
+   lightShadowAttenuation = 1;
    #else
    
-   half4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+   half4 shadowCoord = TransformWorldToShadowCoord(worldPosition);
    Light mainLight = GetMainLight(shadowCoord);
-   Direction = mainLight.direction;
-   Color = mainLight.color;
-   DistanceAtten = mainLight.distanceAttenuation;
-   ShadowAtten = mainLight.shadowAttenuation;
+   lightDirection = mainLight.direction;
+   lightColor = mainLight.color;
+   lightDistanceAttenuation = mainLight.distanceAttenuation;
+   lightShadowAttenuation = mainLight.shadowAttenuation;
    
    #endif
 }
